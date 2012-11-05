ce92f75fb6d0e0e4fb01091cc1a3ce04d5c14582
fnobi <shin@fnobi.com>
2012-11-05 16:41:45 +0900

    sub stateに対応。

--- public/javascripts/StateMachine.js
+++ public/javascripts/StateMachine.js
@@ -2,57 +2,88 @@
   StateMachine - 状態遷移機械の実装
 ============================================================================= */
 
-// ひとつの状態を持つ状態遷移機械
-var StateMachine = function () {
-	this.state = this.initialState = new this.State('initial');
+var StateMachine = (function () {
+	// ひとつの状態を持つ状態遷移機械
+	var StateMachine = function () {
+		this.state = this.initialState = new this.State('initial');
 
-	this.Transition.prototype.stateMachine = this;
-	this.transitions = [];
-};
+		this.Transition.prototype.stateMachine =
+		this.Condition .prototype.stateMachine =
+		this.State     .prototype.stateMachine = this;
 
-// 状態遷移機械にconditionを与えて、状態を遷移させる
-StateMachine.prototype.transit = function (condition) {
-	var stateTo = this.findStateTo(condition);
+		this.transitions = [];
+	};
 
-	if (!stateTo) {
-		throw new Error('no state to transit to.');
-	}
+	// 状態遷移機械にconditionを与えて、状態を遷移させる
+	StateMachine.prototype.transit = function (condition) {
+		var stateTo = this.findStateTo(condition);
 
-	this.state = stateTo;
-};
-
-// この状態遷移機械が、conditionを与えたらどのstateに進むかを計算
-StateMachine.prototype.findStateTo = function (condition) {
-	var self = this;
-	var stateTo = null;
-
-	this.transitions.forEach(function (transition) {
-		if (
-			transition.stateFrom == self.state
-			&&
-			transition.condition == condition
-		) {
-			stateTo = transition.stateTo;
+		if (!stateTo) {
+			throw new Error('no state to transit to.');
 		}
-	});
 
-	return stateTo;
-};
+		this.state = stateTo;
+	};
 
-// 状態を表すクラス
-StateMachine.prototype.State = function (name) {
-	this.name = name;
-};
+	// この状態遷移機械が、conditionを与えたらどのstateに進むかを計算
+	StateMachine.prototype.findStateTo = function (condition) {
+		var self = this;
+		var stateTo = null;
 
-// 環境を表すクラス
-StateMachine.prototype.Condition = function () {
-};
+		this.transitions.forEach(function (transition) {
+			if (
+				transition.stateFrom.hasState(self.state)
+				&&
+				transition.condition == condition
+			) {
+				stateTo = transition.stateTo;
+			}
+		});
 
-// 状態遷移のセット (遷移前の状態・環境・遷移後の状態のセット) を表すクラス
-StateMachine.prototype.Transition = function (stateFrom, condition, stateTo) {
-	this.stateFrom = stateFrom;
-	this.condition = condition;
-	this.stateTo   = stateTo;
+		return stateTo;
+	};
 
-	this.stateMachine.transitions.push(this);
-};
+	// 状態を表すクラス
+	var State = function (name) {
+		this.name = name;
+		this.subStates = [];
+	};
+	StateMachine.prototype.State = State;
+
+	State.prototype.createSubState = function (name) {
+		var subState = new State(name);
+		subState.parentState = this;
+		this.subStates.push(subState);
+		return subState;
+	};
+
+	State.prototype.hasState = function (another) {
+		if (another == this) {
+			return true;
+		}
+
+		var result = false;
+		this.subStates.forEach(function (subState) {
+			result = result || subState.hasState(another);
+		});
+
+		return result;
+	};
+
+	// 環境を表すクラス
+	var Condition = function () {
+	};
+	StateMachine.prototype.Condition = Condition;
+
+	// 状態遷移のセット (遷移前の状態・環境・遷移後の状態のセット) を表すクラス
+	var Transition = function (from, condition, to) {
+		this.stateFrom = from;
+		this.condition = condition;
+		this.stateTo   = to;
+
+		this.stateMachine.transitions.push(this);
+	};
+	StateMachine.prototype.Transition = Transition;
+
+	return StateMachine;
+})();
--- test/statemachine-test.js
+++ test/statemachine-test.js
@@ -5,7 +5,7 @@ buster.testCase('state machine', {
 
 	'initial state': function () {
 		assert.equals(
-			new StateMachine().state.name,
+			(new StateMachine()).state.name,
 			'initial'
 		);
 	},
@@ -14,13 +14,65 @@ buster.testCase('state machine', {
 		var stateName = 'hoge';
 		var stateMachine = new StateMachine();
 
-		var s = new stateMachine.State(stateName);
-		var c = new stateMachine.Condition();
+		var s1 = stateMachine.initialState;
+		var c  = new stateMachine.Condition();
+		var s2 = new stateMachine.State(stateName);
 
-		new stateMachine.Transition(stateMachine.initialState, c, s);
+		new stateMachine.Transition(s1, c, s2);
 
 		stateMachine.transit(c);
 
 		assert.equals(stateMachine.state.name, stateName);
+	},
+
+	'sub state': function () {
+		var stateMachine = new StateMachine();
+
+		var s  = new stateMachine.State();
+		var ss = s.createSubState();
+
+		assert.equals(ss.parentState, s);
+		assert.equals(s.subStates[0], ss);
+	},
+
+	'has state': function () {
+		var stateMachine = new StateMachine();
+
+		var s   = new stateMachine.State();
+		var ss  = s.createSubState();
+		var sss = ss.createSubState();
+
+		assert(s.hasState(s));
+		assert(s.hasState(ss));
+		assert(s.hasState(sss));
+
+		refute(ss.hasState(s));
+		assert(ss.hasState(ss));
+		assert(ss.hasState(sss));
+
+		refute(sss.hasState(s));
+		refute(sss.hasState(ss));
+		assert(sss.hasState(sss));
+	},
+
+	'transition to sub state': function () {
+		var stateMachine = new StateMachine();
+
+		var s1 = stateMachine.initialState;
+		var s2 = s1.createSubState('substate of initial');
+		var s3 = new stateMachine.State('2nd state');
+
+		var c1  = new stateMachine.Condition();
+		var c2  = new stateMachine.Condition();
+
+		new stateMachine.Transition(s1, c1, s2);
+		new stateMachine.Transition(s1, c2, s3);
+
+		stateMachine.transit(c1);
+		assert.equals(stateMachine.state.name, 'substate of initial');
+
+		stateMachine.transit(c2);
+		assert.equals(stateMachine.state.name, '2nd state');
 	}
+
 });
