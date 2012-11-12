0ec4e74c21e2f6a374e992ccf78182782af62ec8
fnobi <shin@fnobi.com>
2012-11-12 21:51:49 +0900

    ディレクトリ設計・サーバーサイドの最低限の機能を完成。

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
+var resources = require('resources');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'sugoroku' });
+};
+
+exports.editor = function(req, res){
+	var resourceName = req.params[0];
+
+	res.render('editor', {
+		title: resourceName,
+		resourcePath: '/codes/' + resourceName + '.js'
+	});
+};
+
+exports.viewer = function(req, res){
+	var resourceName = req.params[0];
+
+	res.render('viewer', {
+		title: resourceName,
+		resourcePath: '/codes/' + resourceName + '.js'
+	});
+};
+
+exports.codes = function (req, res) {
+	var resourceName = req.params[0];
+
+	resources.read(resourceName, function (err, result) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+
+		res.end('var stateRoot = ' + result + ";");
+	});
 };
